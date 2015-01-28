79e626c35e943fed96264160febb56f0e2a3ae4c
zhifeng <zhifeng@zhifeng-PC.lan>
2015-01-28 15:29:46 +0800

    将数据库改为mysql，修改登录验证过程

--- bim2.0/WebContent/js/login.js
+++ bim2.0/WebContent/js/login.js
@@ -1,14 +1,18 @@
 $(function(){
 	
 	$("#loginbt").click(function(){
-		if($("#username").val()=="") {
+		if($("#username").val()==""||$("#username").val()=="请输入用户名") {
 			alert("请填写用户名！");
 		}
-		else if ($("#password").val()==""){
+		else if ($("#password").val()==""||$("#password").val()=="请输入密码"){
 			alert("请填写密码！");
 		}
 		else {
 			$("#form").submit();
 		}
 	});
+	
+	if ($("#msg").val()!="") {
+		alert($("#msg").val())
+	}
 });
